7ab0ca21a809497a5c8f7cb285e91f4df15b736f
Flames <isaacomenuche@gmail.com>
2024-08-07 06:11:38 +0100

    Add wallet

--- client/src/app/links/page.tsx
+++ client/src/app/links/page.tsx
@@ -1,15 +1,17 @@
 import Header from "@/components/Header";
 import Links from "@/components/links/Links";
+import Wallet from "@/components/Wallet";
 
 export default function Home() {
   return (
-    <div className="min-h-screen sm:p-6 w-full max-w-[1440px] mx-auto">
+    <div className="min-h-screen sm:p-6 w-full max-w-[1440px] mx-auto relative">
       <Header />
       <div className="sm:p-0 sm:pt-6 p-4">
         <div className="">
           <Links />
         </div>
       </div>
+      <Wallet />
     </div>
   );
 }
--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,13 +1,15 @@
 import Header from "@/components/Header";
 import Preview from "@/components/Preview";
+import Wallet from "@/components/Wallet";
 
 export default function Home() {
   return (
-    <div className="bg-gray-light sm:p-6 w-full max-w-[1440px] mx-auto">
+    <div className="bg-gray-light sm:p-6 w-full max-w-[1440px] mx-auto relative">
       <Header />
       <div className="sm:p-0 p-4 bg-white">
         <Preview />
       </div>
+      <Wallet />
     </div>
   );
 }
