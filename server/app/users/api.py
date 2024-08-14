09ddb18900af815099c517008f74a30bea02bf2f
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-08-14 19:58:41 +0100

    quick fix for update

--- server/app/users/api.py
+++ server/app/users/api.py
@@ -24,13 +24,14 @@ async def get_user_by_uuid(user_id: str, users: UsersCRUD = Depends(get_users_cr
     )
 
 
-@router.put("/{user_id}", response_model=UserRead, status_code=http_status.HTTP_200_OK)
+@router.patch("/{user_id}", response_model=UserRead, status_code=http_status.HTTP_200_OK)
 async def update_user_by_uuid(user_id: str, data: UserUpdate, users: UsersCRUD = Depends(get_users_crud)):
     """Hello"""
     try:
-        updated_user = await users.put(user_id, data)
+        updated_user = await users.patch(user_id, data)
         return updated_user
     except HTTPException as e:
         raise e
     except Exception as e:
-        raise HTTPException(status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+        raise HTTPException(
+            status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
