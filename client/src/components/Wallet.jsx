a91e0ad73513522ed040ee4bcb3747e1a05de890
Flames <isaacomenuche@gmail.com>
2024-08-15 05:06:05 +0100

    add create user post request.

--- client/src/components/Wallet.jsx
+++ client/src/components/Wallet.jsx
@@ -5,56 +5,15 @@ import { FaInfo } from "react-icons/fa";
 import { useWallet } from "@/utils/wallet";
 
 export default function Wallet() {
-  const [showDetails, setShowDetails] = useState(false);
   const { connectWallet, disconnectWallet, userAddress, userData } =
     useWallet();
 
-  const handleDetails = () => {
-    if (userData) {
-      setShowDetails(!showDetails);
-    } else {
-      connectWallet();
-    }
-  };
-
   return (
-    <div className="fixed bottom-4 sm:left-6 left-4">
-      <button onClick={handleDetails} className={`w-fit hS`}>
-        <span
-          className={`hidden md:block button text-white  ${
-            userData ? "bg-base-dark" : "bg-base-normal hover:bg-base-dark"
-          }`}
-        >
-          {userData
-            ? `${userAddress.slice(0, 5)}...${userAddress.slice(-5)}`
-            : userAddress}
-        </span>
-        <span
-          className={`block md:hidden rounded-full p-2 text-base-dark ${
-            userData ? "bg-base-normal" : "bg-base-normal"
-          }`}
-        >
-          <FaInfo className="size-4" />
-        </span>
-      </button>
-      <div
-        className={`${
-          showDetails ? "flex" : "hidden"
-        } flex-col items-center gap-4 bg-white`}
-      >
-        <div className="bg-gray-preview size-24 rounded-full"></div>
-        <div className=" flex flex-col items-center gap-[13px]">
-          <div className="bg-gray-preview w-40 h-4 rounded-full"></div>
-          <div className="bg-gray-preview w-[72px] h-2 rounded-full"></div>
-        </div>
-        <button
-          onClick={() => disconnectWallet()}
-          className="flex items-center gap-2 button"
-        >
-          <span className="">Disconnect</span>
-          <FaPowerOff className="size-4" />
-        </button>
-      </div>
+    <div
+      onClick={() => connectWallet()}
+      className="fixed bottom-4 sm:left-6 left-4"
+    >
+      {userAddress}
     </div>
   );
 }
--- client/src/utils/wallet.ts
+++ client/src/utils/wallet.ts
@@ -1,10 +1,25 @@
 import { useState, useEffect } from "react";
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 import axios from "axios";
+import { setAdminToken, isAdmin, clearAdminToken } from "./cookie";
+const apiUrl: string = process.env.NEXT_PUBLIC_API_URL || "";
+
+interface UserProfile {
+  stxAddress: {
+    mainnet: string;
+  };
+  walletProvider: string;
+}
+
+interface UserData {
+  profile: UserProfile;
+}
 
 export const useWallet = () => {
-  const [userData, setUserData] = useState(null);
-  const [userAddress, setUserAddress] = useState("Connect Wallet");
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [userAddress, setUserAddress] = useState<string | "Connect Wallet">(
+    "Connect Wallet"
+  );
 
   const appConfig = new AppConfig(["store_write", "publish_data"]);
   const userSession = new UserSession({ appConfig });
@@ -29,24 +44,50 @@ export const useWallet = () => {
     userSession.signUserOut();
     setUserData(null);
     setUserAddress("Connect Wallet");
+    clearAdminToken();
   };
 
   useEffect(() => {
-    if (userSession.isSignInPending()) {
-      userSession
-        .handlePendingSignIn()
-        .then((userData: any) => {
+    const postUserData = (userData: UserData) => {
+      return axios.post(apiUrl + "/api/v1/users/", {
+        auth_type: "STX Wallet",
+        stx_address_mainnet: userData.profile.stxAddress.mainnet,
+        wallet_provider: userData.profile.walletProvider,
+      });
+    };
+
+    const handleSignIn = async () => {
+      if (userSession.isSignInPending()) {
+        try {
+          const userData = await userSession.handlePendingSignIn();
           setUserData(userData);
           setUserAddress(userData.profile.stxAddress.mainnet);
-        })
-        .catch((error) => {
-          console.error("Error handling pending sign-in:", error);
-        });
-    } else if (userSession.isUserSignedIn()) {
-      const loadedUserData: any = userSession.loadUserData();
-      setUserData(loadedUserData);
-      setUserAddress(loadedUserData.profile.stxAddress.mainnet);
-    }
+
+          const response = await postUserData(userData);
+          setAdminToken(response.data.uuid);
+        } catch (error) {
+          console.error(
+            "Error handling pending sign-in or saving user data:",
+            error
+          );
+        }
+      } else if (userSession.isUserSignedIn()) {
+        const userData = userSession.loadUserData();
+        setUserData(userData);
+        setUserAddress(userData.profile.stxAddress.mainnet);
+
+        if (!isAdmin()) {
+          try {
+            const response = await postUserData(userData);
+            setAdminToken(response.data.uuid);
+          } catch (error) {
+            console.error("Error saving user data:", error);
+          }
+        }
+      }
+    };
+
+    handleSignIn();
   }, []);
 
   return {
