dfaba15f226cbb1cc77a2829795bfb7ab4c3500a
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-08-15 04:34:32 +0100

    feat: profile picture upload

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-instrument bg-gray-light `}>{children}</body>
+      <body className={`font-instrument bg-gray-light `}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
