0cc9f7e7eacc17622c8cd8da47a8cfa205c77dbc
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-08-15 02:25:20 +0100

    supabase auth done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/Header";
+import { cookies } from "next/headers";
 
 export default function Home() {
+  const cookieStore = cookies();
   return (
     <div className="bg-gray-light sm:p-6 w-full max-w-[1440px] mx-auto">
       <Header />
       <div className="sm:p-0 p-4 bg-white">Yooo</div>
+      <div>{cookieStore.get("uuid")?.value} | Hello</div>
     </div>
   );
 }
