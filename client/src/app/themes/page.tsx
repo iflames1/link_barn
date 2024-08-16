2abf842dafdf6fced62ba9ab5f250b2326bc872e
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-08-16 05:54:48 +0100

    fix: themes

--- client/src/app/themes/page.tsx
+++ client/src/app/themes/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import ThemesPreview from "@/components/preview/theme-preview";
 import ThemesPreviewButton from "@/components/preview/theme-preview-button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -5,23 +6,34 @@ import { themes } from "@/data/themes";
 
 const Themes = () => {
   return (
-    <Tabs className="grid grid-cols-2">
-      {/* <ThemesPreview key={index} theme={theme} /> */}
-      <TabsList className="grid grid-cols-2 md:grid-cols-3 max-w-[700px] gap-3">
-        {themes.map((theme, index) => (
-          <TabsTrigger key={index} value={theme.name} asChild>
-            <ThemesPreviewButton theme={theme} />
-          </TabsTrigger>
-        ))}
-      </TabsList>
-      <div>
-        {themes.map((theme, index) => (
-          <TabsContent value={theme.name} key={index}>
-            <ThemesPreview theme={theme} />
-          </TabsContent>
-        ))}
-      </div>
-    </Tabs>
+    <div className="min-h-screen sm:p-6 w-full max-w-[1440px] mx-auto relative">
+      <Header />
+      <Tabs className="grid grid-cols-2 gap-6 mt-12">
+        <TabsList className="grid grid-cols-2 md:grid-cols-3 bg-transparent max-w-[700px] gap-4">
+          {themes.map((theme, index) => (
+            <TabsTrigger
+              key={index}
+              value={theme.name}
+              asChild
+              className={`data-[state=active]:${theme.background.value}`}
+            >
+              <ThemesPreviewButton theme={theme} />
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        <div>
+          {themes.map((theme, index) => (
+            <TabsContent
+              value={theme.name}
+              className={`data-[state=active]:${theme.background.value}`}
+              key={index}
+            >
+              <ThemesPreview theme={theme} />
+            </TabsContent>
+          ))}
+        </div>
+      </Tabs>
+    </div>
   );
 };
 
