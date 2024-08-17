cf250e1ef0f203fcecfc055c91b88889b1723bb0
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-08-18 00:57:27 +0100

    feat: added two endpoints get userprofile and check

--- server/app/users/api.py
+++ server/app/users/api.py
@@ -4,7 +4,7 @@ from app.users.crud import UsersCRUD
 from app.users.deps import get_users_crud
 from app.links.models import LinkRead as LinkReadModel
 from app.core.models import StatusMessage
-from app.users.models import UserRead, UserCreate, UserUpdate, User
+from app.users.models import UserRead, UserCreate, UserUpdate, User, CheckRequest, UserProfile, Preview
 
 router = APIRouter()
 
@@ -26,18 +26,35 @@ async def get_user_by_uuid(user_id: str, users: UsersCRUD = Depends(get_users_cr
     # )
 
 
-@router.get("/is_signed_up/{stx_address_mainnet}", response_model=StatusMessage, status_code=http_status.HTTP_200_OK)
-async def check_user_exists(stx_address_mainnet: str, users: UsersCRUD = Depends(get_users_crud)):
+# @router.get("/is_signed_up/{stx_address_mainnet}", response_model=StatusMessage, status_code=http_status.HTTP_200_OK)
+# async def check_user_exists(stx_address_mainnet: str, users: UsersCRUD = Depends(get_users_crud)):
+#     try:
+#         user = await users.check_user_is_created(stx_address_mainnet)
+#         return StatusMessage(status=True, message="User already exists")
+#     except HTTPException as e:
+#         if e.status_code == http_status.HTTP_404_NOT_FOUND:
+#             return {"status": False, "message": "User not found"}
+#         raise e
+#     except Exception as e:
+#         raise HTTPException(status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR,
+#                             detail=f"An error occured: {str(e)}")
+
+
+@router.post("/check", response_model=StatusMessage, status_code=http_status.HTTP_200_OK)
+async def check(data: CheckRequest, users: UsersCRUD = Depends(get_users_crud)):
     try:
-        user = await users.check_user_is_created(stx_address_mainnet)
-        return StatusMessage(status=True, message="User already exists")
-    except HTTPException as e:
-        if e.status_code == http_status.HTTP_404_NOT_FOUND:
-            return {"status": False, "message": "User not found"}
-        raise e
+        exists = await users.check_field_exists(field=data.field, value=data.value)
+        return StatusMessage(status=exists,
+                             message=f"{data.field.capitalize()} {data.value} {'already exists' if exists else 'does not exist'}")
     except Exception as e:
         raise HTTPException(status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail=f"An error occured: {str(e)}")
+                            detail=f"An error occurred: {str(e)}")
+
+
+@router.get("/profile/{username}", response_model=Preview, status_code=http_status.HTTP_200_OK)
+async def get_user_profile(username: str, users: UsersCRUD = Depends(get_users_crud)):
+    user = await users.get_user_profile(username)
+    return UserProfile.from_orm(user)
 
 
 @router.get("/all", response_model=List[User], status_code=http_status.HTTP_200_OK)
