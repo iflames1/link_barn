40d4cbf0c841fb91482d6e2d373bd571f2dac46d
Flames <isaacomenuche@gmail.com>
2024-08-19 20:53:53 +0100

    update middleware

--- client/src/app/[username]/page.tsx
+++ client/src/app/[username]/page.tsx
@@ -1,8 +1,51 @@
+"use client";
+import PreviewLinks from "@/components/preview/preview-links";
+import PreviewProfile from "@/components/preview/preview-profile";
+import { API_BASE_URL } from "@/lib/constants";
+import { useLinkSync } from "@/utils/linkSync";
+import { useWallet } from "@/utils/wallet";
+import { useEffect, useState } from "react";
+import Loading from "../loading";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 interface PageProps {
   params: { username: string };
 }
 
 export default function Page({ params }: PageProps) {
   const { username } = params;
-  return <div>Username: {username}</div>;
+  const { getLinks, links, userProfileDetails } = useLinkSync();
+  const { checkUserExists } = useWallet();
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const res = await checkUserExists(username);
+      if (res.status) {
+        console.log("User exists");
+        getLinks(`${API_BASE_URL}/users/?user_id=${username}`);
+      } else {
+        console.log("first");
+        router.push("/");
+      }
+    };
+    setLoading(true);
+    checkUser();
+    setLoading(false);
+  }, [checkUserExists, getLinks, username]);
+
+  return (
+    <>
+      {loading && <Loading />}
+      <div
+        suppressHydrationWarning={true}
+        className="w-full h-full px-[18%] pt-[53px] flex flex-col items-center gap-14"
+      >
+        <PreviewProfile userProfileDetails={userProfileDetails} />
+        <PreviewLinks links={links} />
+      </div>
+    </>
+  );
 }
