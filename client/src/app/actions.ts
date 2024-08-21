d9c9334a49c5e07be13277c8193350279bdc4dfa
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-08-21 23:06:12 +0100

    fix

--- client/src/app/actions.ts
+++ client/src/app/actions.ts
@@ -1,6 +1,11 @@
 "use server";
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function revalidatePathServer(path: string) {
   revalidatePath(`/${path}`);
+}
+
+export async function revalidateTagServer(tag: string) {
+  console.log("CALLED");
+  revalidateTag(tag);
 }
