9fcfc2651b6167b8775423d6df06441a332ca8b5
Flames <isaacomenuche@gmail.com>
2024-08-24 00:35:00 +0100

    add logo to login page

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -5,12 +5,21 @@ import ConnectWallet from "./connect-wallet";
 import { Submit } from "./sumbit";
 import { NextResponse } from "next/server";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getUserUUID } from "@/lib/auth";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Login = () => {
+  const [showText, setShowText] = useState(false);
   const router = useRouter();
   const uuid = getUserUUID();
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowText(true), 500);
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     if (uuid) {
@@ -21,11 +30,36 @@ const Login = () => {
   }, [uuid, router]);
 
   return (
-    <div className="flex items-center justify-center h-dvh flex-col gap-4 !w-full">
-      <form action={signInWithGoogle} className="w-full max-w-[300px]">
-        <Submit />
-      </form>
-      <ConnectWallet />
+    <div className="h-dvh w-full flex flex-col items-center justify-center gap-16">
+      <Link href={"/"} className="flex items-center gap-2">
+        <motion.div
+          initial={{ x: 50 }}
+          animate={{ x: showText ? 0 : 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Image
+            src={"/unik.png"}
+            alt=""
+            width={40}
+            height={40}
+            className="rounded-lg"
+          />
+        </motion.div>
+        <motion.p
+          initial={{ opacity: 0, x: 0 }}
+          animate={{ opacity: showText ? 1 : 0, x: showText ? 0 : 20 }}
+          transition={{ duration: 0.5 }}
+          className="text-center hM"
+        >
+          Link Barn
+        </motion.p>
+      </Link>
+      <div className="flex items-center justify-center flex-col gap-4 !w-full">
+        <form action={signInWithGoogle} className="w-full max-w-[300px]">
+          <Submit />
+        </form>
+        <ConnectWallet />
+      </div>
     </div>
   );
 };
