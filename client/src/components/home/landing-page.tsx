9f8aac5ca3d0352fa8b3ca3855dcda2e24842549
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-08-23 07:45:16 +0100

    feat: testimonials to home page

--- client/src/components/home/landing-page.tsx
+++ client/src/components/home/landing-page.tsx
@@ -7,12 +7,25 @@ import { TestimonialLamp } from "./lamp-testimonial";
 import { HeroScrollDemo } from "./container-scroll";
 import { FeaturesSectionDemo } from "./feats";
 import { BorderBeam } from "../ui/border-beam";
+import { Testimonials } from "./testimonials";
+import { CustomCursor } from "../ui/custom-cursor";
 
 export function LandingPage() {
   return (
     <div className="h-full">
       <Hero />
-      <FeaturesSectionDemo />
+      <div className="md:h-[50rem] lg:h-[40rem] w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <FeaturesSectionDemo />
+      </div>
+      <Testimonials />
+      {/* </div> */}
+      {/* <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"> */}
+      {/*   <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"> */}
+      {/*     Border Beam */}
+      {/*   </span> */}
+      {/*   <BorderBeam size={250} duration={12} delay={9} /> */}
+      {/* </div> */}
       {/*<TestimonialLamp />*/}
       {/*<Features />*/}
       {/*preview scroll is not working as expected*/}
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette");
 
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
 const config = {
   darkMode: ["class"],
   content: [
@@ -89,15 +92,62 @@ const config = {
             "offset-distance": "100%",
           },
         },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+        shimmer: {
+          "0%, 90%, 100%": {
+            "background-position": "calc(-100% - var(--shimmer-width)) 0",
+          },
+          "30%, 60%": {
+            "background-position": "calc(100% + var(--shimmer-width)) 0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+        shimmer: "shimmer 8s infinite",
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), addVariablesForColors],
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+            )}")`,
+          }),
+        },
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: "color",
+        },
+      );
+    },
+  ],
 } satisfies Config;
 
 function addVariablesForColors({ addBase, theme }: any) {
