a69019a86902d461c43dc43e4b68d59d1262d68c
Flames <isaacomenuche@gmail.com>
2024-09-02 02:40:10 +0100

    add unik holder check

--- client/src/utils/wallet.ts
+++ client/src/utils/wallet.ts
@@ -4,6 +4,7 @@ import { API_BASE_URL } from "@/lib/constants";
 import { useAppContext } from "@/context";
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 import { useState } from "react";
+import { toast } from "sonner";
 
 interface UserProfile {
   stxAddress: {
@@ -137,6 +138,25 @@ export const useWallet = () => {
     }
   };
 
+  const holdUnik = async (
+    principal: string = userAddress
+  ): Promise<boolean> => {
+    try {
+      const response = await axios.get(
+        `https://api.hiro.so/extended/v1/tokens/nft/holdings?principal=${principal}&asset_identifiers=SP3X27NM39MR9HM98D8PEWAHE420JK3X090S1382Q.unikind::unikind&limit=1&unanchored=false`
+      );
+
+      if (response.data && response.data.total > 0) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error("Error checking Unik holdings:", error);
+      toast.error("Error checking if user holds unik");
+      return false;
+    }
+  };
+
   return {
     userData,
     userAddress,
@@ -145,5 +165,6 @@ export const useWallet = () => {
     checkUserExists,
     handleConnect,
     pending,
+    holdUnik,
   };
 };
