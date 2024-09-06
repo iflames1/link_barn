05bdf24cf8da6481cacc6d82383f82d6dec1029a
flames <isaacomenuche@gmail.com>
2024-09-06 21:48:12 +0100

    correct height

--- client/src/components/links/new-preview.tsx
+++ client/src/components/links/new-preview.tsx
@@ -22,7 +22,7 @@ export const NewPreview = ({
   return (
     <div>
       <div
-        className={`lg:flex p-6 rounded-xl bg-white justify-center items-center h-[calc(100vh-152px)] `}
+        className={`lg:flex p-6 rounded-xl bg-white justify-center items-center sm:h-[calc(100vh-152px)] h-[calc(100vh-96.37px)] `}
       >
         <div className="lg:max-h-[calc(100vh-192px)] overflow-y-auto overflow-x-hidden">
           <div className="border-[1px] border-gray-dark rounded-[56px] max-w-[100vw] sm:w-[307px] h-[631px] p-[11px] relative">
@@ -108,7 +108,7 @@ const NewUserProfile = ({
                   width={104}
                   height={104}
                   className={cn(
-                    "rounded-full border-4 size-28 border-base-dark object-cover",
+                    "rounded-full border-4 size-28 border-base-dark object-cover"
                   )}
                 />
               ) : (
