4e91b66f2ffdb5a6ab6ef58fcb1b29d77599e5ff
flames <isaacomenuche@gmail.com>
2024-09-08 12:04:07 +0100

    remove appearance from theme

--- client/src/components/themes/theme-collection.tsx
+++
@@ -1,7 +0,0 @@
-export default function ThemeCollection() {
-  return (
-    <div className="bg-white rounded-xl sm:h-[calc(100vh-152px)] h-[calc(100vh-96.37px)] overflow-auto lg:w-[60%]">
-      theme-collection
-    </div>
-  );
-}
