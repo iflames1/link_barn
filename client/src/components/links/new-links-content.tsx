20afbd0c74cb2f8f9fc81689d43966196833f3c1
flames <isaacomenuche@gmail.com>
2024-09-10 10:56:53 +0100

    increase width

--- client/src/components/links/new-links-content.tsx
+++ client/src/components/links/new-links-content.tsx
@@ -287,7 +287,7 @@ export const NewLinks = ({
         </Preview>*/}
       <Preview userProfileDetails={userProfile} />
 
-      <div className="bg-white flex flex-col justify-between rounded-xl z-0 sm:h-[calc(100vh-152px)] h-[calc(100vh-96.37px)] overflow-auto">
+      <div className="bg-white flex flex-col justify-between rounded-xl z-0 sm:h-[calc(100vh-152px)] h-[calc(100vh-96.37px)] overflow-auto w-full">
         <div className="sm:p-10 p-6">
           <div className="pb-6  relative">
             <h2 className="hM text-black pb-2">Customize your links</h2>
