a502d8536fbddb18b1fe6cb96cafec19dd58765b
flames <isaacomenuche@gmail.com>
2024-09-11 11:03:08 +0100

    validate wallet

--- client/src/lib/holdsUnik.ts
+++ client/src/lib/holdsUnik.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
+import { toast } from "sonner";
 
-export const holdUnik = async (principal: string) => {
+export const holdsUnik = async (principal: string | undefined) => {
+  if (!principal) {
+    console.log("No principal found");
+    toast.error("Failed to check if you holder UNIKING NFT", {
+      richColors: true,
+    });
+    return false;
+  }
+
   const url = `https://api.hiro.so/extended/v1/tokens/nft/holdings?principal=${principal}&asset_identifiers=SP3X27NM39MR9HM98D8PEWAHE420JK3X090S1382Q.unikind::unikind&limit=1&unanchored=false`;
 
   try {
