52fbaee658242e21c9fc88fc3c5cbff88d923021
flames <isaacomenuche@gmail.com>
2024-09-11 02:32:03 +0100

    make amount whole

--- client/src/utils/wallet.ts
+++ client/src/utils/wallet.ts
@@ -28,7 +28,7 @@ interface UserData {
 export const useWallet = () => {
   const [userData, setUserData] = useState<UserData | null>(null);
   const [userAddress, setUserAddress] = useState<string | "Connect Wallet">(
-    "Connect Wallet",
+    "Connect Wallet"
   );
   const [pending, setPending] = useState(false);
 
@@ -86,7 +86,7 @@ export const useWallet = () => {
 
   const checkUserExists = async (
     field: string = "username",
-    value: string,
+    value: string
   ): Promise<{ status: boolean; message: string }> => {
     try {
       const response = await axios.post(`${API_BASE_URL}/users/check`, {
@@ -156,11 +156,11 @@ export const useWallet = () => {
   };
 
   const holdUnik = async (
-    principal: string = userAddress,
+    principal: string = userAddress
   ): Promise<boolean> => {
     try {
       const response = await axios.get(
-        `https://api.hiro.so/extended/v1/tokens/nft/holdings?principal=${principal}&asset_identifiers=SP3X27NM39MR9HM98D8PEWAHE420JK3X090S1382Q.unikind::unikind&limit=1&unanchored=false`,
+        `https://api.hiro.so/extended/v1/tokens/nft/holdings?principal=${principal}&asset_identifiers=SP3X27NM39MR9HM98D8PEWAHE420JK3X090S1382Q.unikind::unikind&limit=1&unanchored=false`
       );
 
       if (response.data && response.data.total > 0) {
@@ -177,12 +177,12 @@ export const useWallet = () => {
   async function sendSTXTransaction(
     recipientAddress = "SPVT6STGX1AG9E0D8H87HB3N0ZV8SJPCMDMN373D",
     amount = "1",
-    memo = "",
+    memo = ""
   ) {
     const transactionDetails: STXTransferOptions = {
       network: new StacksMainnet(),
       recipient: recipientAddress,
-      amount: amount,
+      amount: amount + "000000",
       memo: memo,
       appDetails: {
         name: "Link Barn",
