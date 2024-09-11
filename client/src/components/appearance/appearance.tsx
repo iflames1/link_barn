d7451069ad09042344b53ea44a6eaf1bf7c298bf
flames <isaacomenuche@gmail.com>
2024-09-11 02:25:20 +0100

    remove comments

--- client/src/components/appearance/appearance.tsx
+++ client/src/components/appearance/appearance.tsx
@@ -19,61 +19,6 @@ const ChangeAppearance = dynamic(() => import("./use-appearance"), {
 import PreviewLayout from "./preview-layout";
 import { sampleUserData } from "@/data/sampleUserData";
 
-//export const sampleUserData: UserData = {
-//  first_name: "Alex",
-//  last_name: "Johnson",
-//  username: "alexj",
-//  email: "",
-//  stx_address_mainnet: "",
-//  uuid: "1",
-//  bio: "Web developer, coffee enthusiast, and part-time adventurer. Building the future one line of code at a time.",
-//  profile_picture: "/dp.jpg",
-//  appearance: "layout1",
-//  theme: "theme1",
-//  links: [
-//    {
-//      uuid: "1",
-//      platform: "Portfolio",
-//      index: 0,
-//      url: "https://example.com",
-//      user_id: "link",
-//      link_title: "portfolio",
-//    },
-//    {
-//      uuid: "2",
-//      platform: "Twitter",
-//      index: 1,
-//      url: "https://example.com",
-//      user_id: "twitter",
-//      link_title: "twitter",
-//    },
-//    {
-//      uuid: "3",
-//      platform: "Instagram",
-//      index: 2,
-//      url: "https://example.com",
-//      user_id: "instagram",
-//      link_title: "instagram",
-//    },
-//    {
-//      uuid: "4",
-//      platform: "LinkedIn",
-//      index: 3,
-//      url: "https://example.com",
-//      user_id: "linkedin",
-//      link_title: "linkedin",
-//    },
-//    {
-//      uuid: "5",
-//      platform: "GitHub",
-//      index: 4,
-//      url: "https://example.com",
-//      user_id: "github",
-//      link_title: "github",
-//    },
-//  ],
-//};
-
 export default function Themes({
   userProfile,
 }: {
@@ -111,16 +56,6 @@ export default function Themes({
         console.log("Transaction Status:", data.tx_status);
 
         handleTx(data.tx_status);
-        //if (data.tx_status === "success") {
-        //  console.log("Transaction confirmed!");
-        //  return "success";
-        //} else if (data.tx_status === "pending") {
-        //  console.log("Transaction is pending...");
-        //  return "pending";
-        //} else {
-        //  console.log("Transaction failed:", data);
-        //  return "failed";
-        //}
       } catch (error) {
         console.error("Error fetching transaction status:", error);
         toast.error("error checking previous transaction status");
