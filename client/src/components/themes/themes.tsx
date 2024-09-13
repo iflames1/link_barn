d9f50c6d340dd8137f69d30a0fc5ca23335bc1bd
flames <isaacomenuche@gmail.com>
2024-09-13 12:04:54 +0100

    update themes preview

--- client/src/components/themes/themes.tsx
+++ client/src/components/themes/themes.tsx
@@ -35,27 +35,25 @@ export default function Themes({
   return (
     <ThemeSelector defaultValue={selectedTheme}>
       <div className="w-full">
-        {themes.map((theme, index) => (
-          <TabsContent
-            key={index}
-            value={theme.name}
-            className={cn(
-              "max-w-80 mx-auto",
-              `${theme.text}
+        <PreviewLayout className="group w-full">
+          {themes.map((theme, index) => (
+            <TabsContent
+              key={index}
+              value={theme.name}
+              className={cn(
+                `${theme.text}
                 data-[state=active]:${theme.bg}
                 data-[state=active]:${theme.text}
                 data-[state=active]:shadow-sm
+                group-active:${theme.bg}
                 transition-all`
-            )}
-            asChild
-          >
-            <PreviewLayout className="max-w-none w-full" bg={theme.bg}>
-              <div className="py-14 px-4">
-                <LayoutComponent userData={sampleUserData} />
-              </div>
-            </PreviewLayout>
-          </TabsContent>
-        ))}
+              )}
+              asChild
+            >
+              <LayoutComponent userData={sampleUserData} />
+            </TabsContent>
+          ))}
+        </PreviewLayout>
       </div>
       <div className="bg-white rounded-xl lg:h-[calc(100vh-152px)] h-[calc(100vh-96.37px)] overflow-y-auto w-full p-6 relative">
         <TabsList className="bg-transparent h-full grid grid-cols-1 sm:grid-cols-2 gap-4">
